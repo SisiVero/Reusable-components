629c96ea13cdefc044b06e73798a0d27878d8f7a
SisiVero <csusan894@gmail.com>
2023-09-10 12:31:09 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Button from "./Button"
+import { FaMoneyBill } from 'react-icons/fa'
+
+function App() {
+  return (
+    <main>
+      <Button>
+        <FaMoneyBill />       
+        Buy now!
+      </Button>
+    </main>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
